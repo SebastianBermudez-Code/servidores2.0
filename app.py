fb362e8334b00f0b093ec672e05edf90a61c14a9
Andrea77714 <camandriu1422@gmail.com>
2024-08-30 10:50:06 -0500

    Flowers

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, redirect
+import db
 
 servidor2 = Flask(__name__)
 
@@ -6,5 +7,34 @@ servidor2 = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@servidor2.route('/productos', methods=('GET', 'POST'))
+def products():
+    if request.method == 'POST':
+        if 'product_id' in request.form:
+            # Update product
+            product_id = request.form['product_id']
+            name = request.form['name']
+            description = request.form['description']
+            price = request.form['price']
+            image = request.form['image']
+            db.update_product(product_id, name, description, price, image)
+        else:
+            # Add product
+            name = request.form['name']
+            description = request.form['description']
+            price = request.form['price']
+            image = request.form['image']
+            db.add_product(name, description, price, image)
+        return redirect(url_for('products'))
+    
+    products = db.get_all_products()
+    return render_template('productos.html', products=products)
+
+@servidor2.route('/delete_product/<int:product_id>', methods=('POST',))
+def delete_product(product_id):
+    db.delete_product(product_id)
+    return redirect(url_for('products'))
+
 if __name__ == '__main__':
+    db.init_db()
     servidor2.run(debug=True)
---
+++ db.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+def get_db_conection():
+    conn = sqlite3.connect('productos.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_conection()
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        description TEXT,
+        price REAL NOT NULL,
+        image TEXT
+    )
+''')
+    conn.commit()
+    conn.close()
+
+def get_all_products():
+    conn = get_db_conection()
+    products = conn.execute('SELECT * FROM products').fetchall()
+    conn.close()
+    return products
+
+def add_product(name, description, price, image):
+    conn = get_db_conection()
+    conn.execute('INSERT INTO products (name, description, price, image) VALUES (?,?,?,?)', (name, description, price, image))
+    conn.commit()
+    conn.close()
+
+def update_product(product_id, name, description, price, image):
+    conn = get_db_conection()
+    conn.execute('''
+        UPDATE products 
+        SET name = ?, description = ?, price = ?, image = ? 
+        WHERE id = ?
+    ''', (name, description, price, image, product_id))
+    conn.commit()
+    conn.close()
+
+
+def delete_product(producto_id):
+    conn = get_db_conection()
+    conn.execute('DELETE FROM products WHERE id = ?', (producto_id,))
+    conn.close()
